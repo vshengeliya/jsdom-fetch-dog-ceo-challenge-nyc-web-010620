0a65c91aa7566d9fdb5b92dda214568dca2e85b7
github username <github email address>
2020-08-08 12:15:06 -0400

    all 4 deliverables

--- src/index.js
+++ src/index.js
@@ -1,77 +1,88 @@
-console.log('%c HI', 'color: firebrick')
-let dogBreeds = [];
+document.addEventListener("DOMContentLoaded", function(){
 
-document.addEventListener('DOMContentLoaded', function(event){
+    console.log('%c HI', 'color: firebrick')
+    
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-
-    fetch(imgUrl)
-    .then (resp =>resp.json())
-    .then (dogs => dogs.message.forEach(addDog))
-    
-    
-    function addDog(dogUrl){
-        const divConrainer = document.getElementById('dog-image-container')
-        const dogImg = document.createElement('img')
-        dogImg.src = dogUrl
-        divConrainer.appendChild(dogImg)
-        
-    }
-
     const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    let array = []
+    let ul = document.querySelector("#dog-breeds")
+    
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(dogs=> dogs.message.forEach(renderDogs))
+    
+    function renderDogs(dog){
+         
+        let divDog = document.querySelector("#dog-image-container")
+        let image = document.createElement("img")
+        image.src = `${dog}`
+        divDog.appendChild(image)
+    }
+
     fetch(breedUrl)
-    .then (resp =>resp.json())
-    // .then((dogs => Object.keys(dogs.message).forEach(breed => dogBreeds.push(breed))))
-    // .then(console.log)
-    .then (dogs => Object.keys(dogs.message).forEach(breed => {
-        // console.log('before add breed')
-        addBreed(breed)
-        // console.log('after add breed')
-        dogBreeds.push(breed)
-        // console.log('after push')
-        // console.log(dogBreeds)
-    }))
+    .then(resp=>resp.json())
+    .then(breeds=>Object.keys(breeds.message).forEach(renderBreeds))
 
-    function addBreed(breed){
-        // console.log(breed)
-        const ul = document.getElementById('dog-breeds')
-        const breedLi = document.createElement('li')
-        breedLi.addEventListener('click', function(event){
+    function renderBreeds(breed){
+        array.push(breed)
+        let li = document.createElement('li')
+        li.innerText=`${breed}`
+        ul.appendChild(li)
 
-         event.target.style.color = 'red'
+        li.addEventListener("click", function(e){
 
+        e.target.style.color = 'turquoise'
         })
-        breedLi.innerText = `${breed}`
-        ul.appendChild(breedLi)
+    }
+      
+    let dropDown  = document.getElementById("breed-dropdown")
+    dropDown.addEventListener("change", function(e){
 
-        // ul.innerHTML = ul.innerHTML + `<li>${breed}</li>`
+       if (e.target.value==='a'){
+            
+           let byA = array.filter(n=> {
+               {return n.split("")[0]==='a';
+               }})
+        
+               ul.innerHTML=""
+               byA.forEach(renderFilteredBreeds)
+
+            } else if (e.target.value==='b'){
+                let byB = array.filter(n=> {
+                    {return n.split("")[0]==='b';
+                    }})
+             
+                    ul.innerHTML=""
+                    byB.forEach(renderFilteredBreeds)
+     
+            } else if (e.target.value==='c'){
+                let byC = array.filter(n=> {
+                    {return n.split("")[0]==='c';
+                    }})
+
+                    ul.innerHTML=""
+                    byC.forEach(renderFilteredBreeds)
        
-    }
+            } else {
+                let byD = array.filter(n=> {
+                    {return n.split("")[0]==='d';
+                    }})
+                    ul.innerHTML=""
+                    byD.forEach(renderFilteredBreeds)
+                
+            }
 
-    const breedDropdown = document.getElementById('breed-dropdown')
-    breedDropdown.addEventListener('change', showFilteredBreeds)
+         function renderFilteredBreeds(breed){
+          let li = document.createElement('li')
+          li.innerText=`${breed}`
+          ul.appendChild(li)
+        }
 
-    function showFilteredBreeds(event) {
-        const letter = event.target.value
-        const filteredBreeds = dogBreeds.filter(breed => breed.charAt(0) === letter);
-        const ul = document.getElementById('dog-breeds')
-        ul.innerHTML=""
-        filteredBreeds.forEach(addBreed)
-    }
-
-
-
-    
-
-
+    })
 
     
-}) // End of DOMContentLoaded
 
 
 
+})//DOMContentLoaded
 
-
-// const result = words.filter(word => {
-//     return word.length > 6
-// });
-// const result = words.filter(function(word) {return word.length > 6})
